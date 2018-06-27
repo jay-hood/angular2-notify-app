0bee1b8cd03c36aa7f3e29df79fc18af49048cad
Jay <hood.jay@gmail.com>
2018-06-27 14:25:36 -0400

    added core module and resize directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './shared/modules/app-routing.module';
 import { HttpModule } from '@angular/http';
 import { SharedModule } from './shared/modules/shared.module';
 import { AuthModule } from './shared/modules/auth.module';
 import { CoreModule } from './shared/modules/core.module';
 
-import { AppRoutingModule } from './shared/modules/app-routing.module';
-
-import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { BulletinBoardComponent } from './bulletin-board/bulletin-board.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BulletinBoardComponent
   ],
   imports: [
     BrowserModule,
