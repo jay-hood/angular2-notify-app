b0d7c6a1a2c0ab32b33066936881eef71cebdf97
Jay <hood.jay@gmail.com>
2018-06-27 14:45:22 -0400

    somehow http module got deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './shared/modules/app-routing.module';
 import { HttpModule } from '@angular/http';
-import { SharedModule } from './shared/modules/shared.module';
-import { AuthModule } from './shared/modules/auth.module';
+import { AppRoutingModule } from './shared/modules/app-routing.module';
 import { CoreModule } from './shared/modules/core.module';
+import { AuthModule } from './shared/modules/auth.module';
+
+import { SharedModule } from './shared/modules/shared.module';
+
 
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './header/header.component';
-
-import { BulletinBoardComponent } from './bulletin-board/bulletin-board.component';
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BulletinBoardComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
     SharedModule,
-    AuthModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
