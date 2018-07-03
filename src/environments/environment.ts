d2a4f1ff75cadef8ff82dd8aea07d3330aab0ce6
Jay <hood.jay@gmail.com>
2018-07-03 13:06:08 -0400

    removed unused import

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyCZr9_nph3wdg-59bOGnFp6U-BHj84XtYs',
-    authDomain: 'ng-notebook-jay.firebaseapp.com',
-    databaseURL: 'https://ng-notebook-jay.firebaseio.com',
-    projectId: 'ng-notebook-jay',
-    storageBucket: 'ng-notebook-jay.appspot.com',
-    messagingSenderId: '155853433485'
-  }
+  production: false
 };
 
 /*
