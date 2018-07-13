e1f6846180afb354273f52d29bbabb31162eab34
Jay <hood.jay@gmail.com>
2018-07-12 21:52:40 -0400

    fixed minor environment variable issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyCZr9_nph3wdg-59bOGnFp6U-BHj84XtYs',
-    authDomain: 'ng-notebook-jay.firebaseapp.com',
-    databaseURL: 'https://ng-notebook-jay.firebaseio.com',
-    projectId: 'ng-notebook-jay',
-    storageBucket: 'ng-notebook-jay.appspot.com',
-    messagingSenderId: '155853433485'
-  }
+  production: true
 };
